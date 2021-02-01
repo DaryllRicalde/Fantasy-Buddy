a9f28cafff5d05362516105fe63893d6bdfd53bf
Alex Li <tianqili0211@gmail.com>
2021-02-01 15:08:18 +1100

    Coverting player name to ID

--- getstats.py
+++ getstats.py
@@ -4,24 +4,36 @@ import requests
 
 def parameter():
     season = input("Which season?")
-    p_ID = input("Which player?")
+    #p_ID = input("Which player?")
+    p_ID = 999999
+    fname = input("Whats the first name?")
+    lname = input("Whats the last name")
+    url = "https://www.balldontlie.io/api/v1/players"
+    response = requests.get(url)
+    data = response.json()
+    query = data["data"]
+    while i < len(query):
+        if fname in query and lname in query:
+            p_ID = query["id"]
     param = "?seasons[]=" + season + "&player_ids[]=" + p_ID + "&postseason=false"
-    print (param)
+    #print (param)
     return param
 
 def getStats():
     url = "https://www.balldontlie.io/api/v1/stats"
     param_url = url + parameter()
-    print(param_url)
+    #print(param_url)
     response = requests.get(param_url)
-
+    p_list = []
     data = response.json()
-    print(data)
+    #print(data)
     i = 0
-    while True:
-        query = data["data"][i]
-        pts = query["pts"]
-        print(pts)
+    query = data["data"]
+    while i < len(query):
+        sub_dict = query[i]
+        pts = sub_dict["pts"]
+        p_list.append(pts)
         i+=1
-
+    #append to some list
+    print(p_list)
 getStats()
