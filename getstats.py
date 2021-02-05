d49ae3f861e76eaed5e1cb1e067f4cd0530784a8
Alex Li <tianqili0211@gmail.com>
2021-02-05 13:25:59 +1100

    getstats.py

--- getstats.py
+++ getstats.py
@@ -3,25 +3,33 @@ import json
 import requests
 
 def parameter():
-    season = input("Which season?")
+    season = input("Which season?\n")
     #p_ID = input("Which player?")
     p_ID = 999999
-    fname = input("Whats the first name?")
-    lname = input("Whats the last name")
-    url = "https://www.balldontlie.io/api/v1/players"
+    fname = input("Whats the first name?\n")
+    lname = input("Whats the last name?\n")
+    fname = fname.lower()
+    lname = lname.lower()
+    url = "https://www.balldontlie.io/api/v1/players?search=" + lname + "&per_page=100"
     response = requests.get(url)
     data = response.json()
     query = data["data"]
-    while i < len(query):
-        if fname in query and lname in query:
-            p_ID = query["id"]
-    param = "?seasons[]=" + season + "&player_ids[]=" + p_ID + "&postseason=false"
+    i = 0
+    while i < len(query):        
+        sub_dict = query[i]
+        if fname == str(sub_dict["first_name"]).lower():            
+            p_ID = sub_dict["id"]
+            print(p_ID)
+                   
+        i+=1
+    param = "?seasons[]=" + str(season) + "&player_ids[]=" + str(p_ID) + "&postseason=false"
+    #print(p_ID)
     #print (param)
     return param
 
 def getStats():
     url = "https://www.balldontlie.io/api/v1/stats"
-    param_url = url + parameter()
+    param_url = url + str(parameter())
     #print(param_url)
     response = requests.get(param_url)
     p_list = []
