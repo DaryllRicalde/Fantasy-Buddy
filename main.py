6ebed6a69bb2b08985ae7a9ac712695b9e84523a
Daryll Ricalde <rstevendaryll@gmail.com>
2021-02-05 14:41:46 +1100

    added labels (temp)

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    
+    labels = [
+        '5','10','15','20','25','30'
+        '35','40','45','50'
+    ]
+    
     return render_template("index.html")
 
 if __name__ == "__main__":
