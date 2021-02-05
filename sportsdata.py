1f11fa9177b3167861002b44109d71a51939744a
Daryll Ricalde <rstevendaryll@gmail.com>
2021-02-05 16:35:44 +1100

    implemented match ID function

--- sportsdata.py
+++ sportsdata.py
@@ -9,6 +9,20 @@ def getLogs():
     api_key = config.api_key
     headers = {'Ocp-Apim-Subscription-Key': '{key}'.format(key=api_key)}
     jsonData = requests.get(url, headers=headers).json()
-    print(jsonData)
+    points = jsonData[0]["Points"]
+    print(data)
 
-getLogs()
+def getPlayer():
+    url = "https://api.sportsdata.io/v3/nba/scores/json/Players"
+    api_key = config.api_key
+    headers = {'Ocp-Apim-Subscription-Key': '{key}'.format(key=api_key)}
+    jsonData = requests.get(url, headers=headers).json()
+    fname = input("What is the player's first name?")
+    lname = input("What is the player's last name?")
+
+    i = 0
+    while i < len(jsonData):
+        if jsonData[i]["FirstName"].lower() == fname.lower() and jsonData[i]["LastName"].lower() == lname.lower():
+            print(jsonData[i]["PlayerID"])
+        i+= 1
+getPlayer()
