b07be313f0b5b6735a61c1d042232d7e71ff602b
Daryll Ricalde <rstevendaryll@gmail.com>
2021-02-08 00:05:24 +1100

    template fixes

--- main.py
+++ main.py
@@ -12,5 +12,10 @@ def index():
     
     return render_template("home.html")
 
+@app.route("/search")
+def search():
+
+    return render_template("search.html")
+
 if __name__ == "__main__":
     app.run()
