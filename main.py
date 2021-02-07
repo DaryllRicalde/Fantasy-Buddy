3eae3f6135615592d983107e583c902dc5c5033b
Daryll Ricalde <rstevendaryll@gmail.com>
2021-02-08 00:52:24 +1100

    commit before branching

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import config
+import json
 
 from flask import Flask, render_template, request
 
@@ -9,13 +10,49 @@ key = config.api_key
 
 @app.route("/")
 def index():
-    
-    return render_template("home.html")
+
+    url = "https://api.sportsdata.io/v3/nba/scores/json/Players"
+    api_key = config.api_key
+    headers = {'Ocp-Apim-Subscription-Key': '{key}'.format(key=api_key)}
+    jsonData = requests.get(url, headers=headers).json()
+
+    i = 0
+    fname = ""
+    lebron_image = ""
+    while i < len(jsonData):
+        if jsonData[i]["FirstName"].lower() == "Lebron" and jsonData[i]["LastName"].lower() == "James":
+            fname = jsonData[i]["FirstName"]
+            lebron_image = jsonData[i]["PhotoUrl"]
+        i+= 1
+
+    return render_template("home.html", fname=fname)
 
 @app.route("/search")
 def search():
 
     return render_template("search.html")
+
+# def getLogs():
+#     url = "https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsBySeason/2020/20000441/all"
+#     api_key = config.api_key
+#     headers = {'Ocp-Apim-Subscription-Key': '{key}'.format(key=api_key)}
+#     jsonData = requests.get(url, headers=headers).json()
+#     points = jsonData[0]["Points"]
+#     print(data)
+
+# def getPlayer():
+#     url = "https://api.sportsdata.io/v3/nba/scores/json/Players"
+#     api_key = config.api_key
+#     headers = {'Ocp-Apim-Subscription-Key': '{key}'.format(key=api_key)}
+#     jsonData = requests.get(url, headers=headers).json()
+#     fname = input("What is the player's first name?")
+#     lname = input("What is the player's last name?")
+
+#     i = 0
+#     while i < len(jsonData):
+#         if jsonData[i]["FirstName"].lower() == fname.lower() and jsonData[i]["LastName"].lower() == lname.lower():
+#             print(jsonData[i]["PlayerID"])
+#         i+= 1
 
 if __name__ == "__main__":
     app.run()
