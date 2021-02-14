3eaf9221a73248586a5db00f0b790e70a8eea824
Daryll Ricalde <rstevendaryll@gmail.com>
2021-02-14 22:32:04 +1100

    commit before testing

--- main.py
+++ main.py
@@ -30,27 +30,40 @@ def player():
     lastName = request.form["lastName"]
 
     playerID = getPlayer(firstName,lastName)
+    p_points = getLogs(playerID)
 
 
     
     return render_template("player.html",
     firstName=firstName,
-    playerID=playerID
+    playerID=playerID,
+    points=p_points
     )
 
-# def getLogs():
-#     url = "https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsBySeason/2020/20000441/all"
-#     api_key = config.api_key
-#     headers = {'Ocp-Apim-Subscription-Key': '{key}'.format(key=api_key)}
-#     jsonData = requests.get(url, headers=headers).json()
-#     points = jsonData[0]["Points"]
-#     print(data)
+def getLogs(playerID):
+    url = "https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsBySeason/2020/" + str(playerID) + "/all"
+    api_key = config.api_key
+    headers = {'Ocp-Apim-Subscription-Key': '{key}'.format(key=api_key)}
+    jsonData = requests.get(url, headers=headers).json()
+    points = []
+
+    data = jsonData[0]
+    data_items = data.items()
+
+    for item in data_items:
+        if item == "Points":
+            points.append(item)
+    
+    # points = jsonData[0]["Points"]
+    return points
 
 def getPlayer(firstName,lastName):
     url = "https://api.sportsdata.io/v3/nba/scores/json/Players"
     api_key = config.api_key
     headers = {'Ocp-Apim-Subscription-Key': '{key}'.format(key=api_key)}
     jsonData = requests.get(url, headers=headers).json()
+
+    playerID = 99999
 
     i = 0
     while i < len(jsonData):
